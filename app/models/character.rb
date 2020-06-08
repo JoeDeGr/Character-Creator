8ed6dcfb8aeff6ef81278934bdc455f40a557678
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-06-08 15:57:59 -0400

    changed stntax in character.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ActiveRecord::Base
     belongs_to :user
     has_many :character_locations
     has_many :locations, through: :character_locations
-    has many :character_types
-    has many :archatypes, through: :character_types
-    has many :powers, through: :archatypes
+    has_many :character_types
+    has_many :archatypes, through: :character_types
+    has_many :powers, through: :archatypes
 end
