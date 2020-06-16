02addc74a2d46aaa4072069ae9535d6642ceaef6
JoeDeGr <DeGregorio.Joseph@gmail.com>
2020-06-16 19:59:51 -0400

    fixed locations/show, created archatype/show

--- app/controllers/archatype_controller.rb
+++ app/controllers/archatype_controller.rb
@@ -8,4 +8,13 @@ class ArchatypeController < ApplicationController
             redirect "/login"
         end 
     end
+    get "/archatypes/:id" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @archatype = Archatype.find_by_id(params[:id])
+            erb :"archatypes/show"
+        else
+            redirect "/login"
+        end
+    end
 end
