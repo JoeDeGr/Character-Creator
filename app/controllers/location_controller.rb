7447220d2aaf77303dbe1eb9681e56fd2b85b437
JoeDeGr <DeGregorio.Joseph@gmail.com>
2020-06-16 19:47:31 -0400

    refactored location/index

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,8 +1,12 @@
 class LocationController < ApplicationController
     get "/locations" do
-        @user = User.find_by_id(session[:users_id])
-        @locations = Location.all
-        erb :"locations/index"
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @locations = Location.all
+            erb :"locations/index"
+        else
+            redirect "/login"
+        end
     end
 
     get "/locations/new" do
@@ -15,5 +19,24 @@ class LocationController < ApplicationController
         else
             redirect "/login"
         end 
+    end
+    get "/locations:id" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @location = Location.find_by_id(params:[id])
+            erb :"locations/show"
+        else
+            redirect "/login"
+        end
+    end
+
+    get "/locations:id/edit" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @location = Location.find_by_id(params:[id])
+            erb :"locations/edit"
+        else
+            redirect "/login"
+        end
     end
 end
