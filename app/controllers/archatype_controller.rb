b00926fa6ae2be007a94a9c88cf2a3ff512a2473
JoeDeGr <DeGregorio.Joseph@gmail.com>
2020-06-17 13:01:25 -0400

    developed archatype/new

--- app/controllers/archatype_controller.rb
+++ app/controllers/archatype_controller.rb
@@ -8,6 +8,17 @@ class ArchatypeController < ApplicationController
             redirect "/login"
         end 
     end
+
+    get "/archatypes/new" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @archatypes = Archatype.all
+            erb :"/archatypes/new"
+        else
+            redirect "/login"
+        end 
+    end
+
     get "/archatypes/:id" do
         if User.is_logged_in?(session)
             @user = User.current_user(session)
@@ -16,6 +27,44 @@ class ArchatypeController < ApplicationController
         else
             redirect "/login"
         end
+    end
+
+    get "/archatypes/:id/edit" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @archatype = archatype.find_by_id(params[:id])
+            erb :"archatypes/edit"
+        else
+            redirect "/login"
+        end
+    end
+
+    post "/archatypes" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @archatype = []
+            @power = []
+            if !!params[:archatype][:name]
+                @archatype = Archatype.create(params[:archatype])
+            end
+            if !!params[:power][:name]
+                @power = Power.create(params[:power])
+                @power.archatype_ids = @archatype.id
+                @power.save
+            end
+            redirect "/archatypes"
+        else
+            redirect "/login"
+        end
+    end
+
+    patch "/archatypes/:id" do
+        @user = User.current_user(session)
+        @archatype = Archatype.find_by_id(params[:id])
+        @archatype.update(params[:archatype])
+        binding.pry
+ 
+        redirect "/locations/#{@location.id}"
     end
 
     delete "/Archatypes/:id" do
