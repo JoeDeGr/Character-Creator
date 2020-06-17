1db5af7a4785515a8bcf9fdd4dc32f797739e5b1
JoeDeGr <DeGregorio.Joseph@gmail.com>
2020-06-17 16:54:11 -0400

    built out powers/edit, powers/index, powers/new, and powers/show. Worked on PowersController.

--- app/controllers/power_controller.rb
+++ app/controllers/power_controller.rb
@@ -1,4 +1,67 @@
 class PowerController < ApplicationController
+    get "/powers" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @powers = Power.all
+            erb :"/powers/index"
+        else
+            redirect "/login"
+        end 
+    end
+
+    get "/powers/new" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @archatypes = Archatype.all
+            erb :"/powers/new"
+        else
+            redirect "/login"
+        end 
+    end
+
+    get "/powers/:id" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @power = Power.find_by_id(params[:id])
+            erb :"/powers/show"
+        else
+            redirect "/login"
+        end
+    end
+
+    get "/powers/:id/edit" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @power = Power.find_by_id(params[:id])
+            @archatypes = Archatype.all
+            erb :"powers/edit"
+        else
+            redirect "/login"
+        end
+    end
+
+    post "/powers" do
+        if User.is_logged_in?(session)
+            @user = User.current_user(session)
+            @power = []
+            if !!params[:power][:name]
+                binding.pry
+                @power = Power.create(params[:power])
+                redirect "/powers"
+            end
+        else
+            redirect "/login"
+        end
+    end
+
+    patch "/powers/:id" do
+        @user = User.current_user(session)
+        @power = Power.find_by_id(params[:id])
+        @power.update(params[:power])
+ 
+        redirect "/powers/#{@power.id}"
+    end
+
     delete "/powers/:id" do
         Power.destroy(params[:id])
         redirect "/powers"
