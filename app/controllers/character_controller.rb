27eec82d2bca4b4729870c0127b923734197b0cd
JoeDeGr <DeGregorio.Joseph@gmail.com>
2020-06-18 18:29:18 -0400

    changed create button in character/save. tried debugging pry.

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -17,7 +17,6 @@ class CharacterController < ApplicationController
     get "/characters/new" do
         if User.is_logged_in?(session)
             @user = User.current_user(session)
-            @characters = @user.characters
             @locations = Location.all
             @archatypes = Archatype.all
             erb :"/characters/new"
@@ -54,26 +53,28 @@ class CharacterController < ApplicationController
     end
 
     post "/characters" do
-
-        @user = User.current_user(session)
-        @character = Character.create(params[:character])
-        location = Location.create(params[:location])
-        building = Building.create(params[:building])
-        archatype = Archatype.create(params[:archatype])
-        power = Power.create(params[:power])
-        location.buildings << building
-        archatype.powers <<  power
-        @character.buildings << building
-        @character.archatypes << archatype
-        @character.locations << location
-        @character.user_id = @user.id
-        @character.save
-
-        redirect "/characters/#{@character.id}"
+        if User.is_logged_in?(session)
+            user = User.current_user(session)
+            @character = Character.create(params[:character])
+            location = Location.create(params[:location])
+            building = Building.create(params[:building])
+            archatype = Archatype.create(params[:archatype])
+            power = Power.create(params[:power])
+            location.buildings << building
+            archatype.powers <<  power
+            @character.buildings << building
+            @character.archatypes << archatype
+            @character.locations << location
+            @character.user_id = user.id
+            @character.save
+            redirect "/characters/#{@character.id}"
+        else
+            redirect "/login"
+        end
     end
 
     patch "/characters/:id" do
-
+        binding.pry
         @user = User.current_user(session)
         @character = Character.find_by_id(params[:id])
         @character.update(params[:character])
