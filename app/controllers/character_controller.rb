d2ffaec07babc93fcbe3d1d2db4f5e4017e826a0
JoeDeGr <DeGregorio.Joseph@gmail.com>
2020-06-24 18:06:24 -0400

    repaired character/new error.

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -19,6 +19,7 @@ class CharacterController < ApplicationController
             @user = User.current_user(session)
             @locations = Location.all
             @archatypes = Archatype.all
+            @powers = Power.all
             erb :"/characters/new"
         else
             redirect "/login"
@@ -31,7 +32,6 @@ class CharacterController < ApplicationController
             @character = Character.find_by_id(params[:id])
             @locations = Location.all
             @archatypes = Archatype.all
-            @powers = Power.all
             @user_page = User.find_by_id(@character.user_id)
             if @user.id == @user_page.id
                 erb :"/characters/edit"
